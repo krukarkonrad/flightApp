9fa529a2a3173cce22cc8dbede694dea15f19032
Konrad <krukarkonrad@gmail.com>
2020-03-12 01:50:07 +0100

    Update

--- flight-app-client/src/App/App.js
+++ flight-app-client/src/App/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import 'antd/dist/antd.css';
 
 import Footer from "../Common/Footer.js"
-import Tourists from "../App/Tourist/Tourists.js"
-import Flight from "../App/Flight/Flights.js"
+import Tourists from "./Tourist/Tourists"
+import Flight from "./Flight/Flights.js"
 import { Tabs } from 'antd/lib';
 import { Layout } from "antd/lib"
 const { Content } = Layout;
